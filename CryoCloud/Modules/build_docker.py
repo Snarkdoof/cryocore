87220c6bcdc0657f8c03ec37d44511cfa3600503
Njål Borch <njaal@njaal.net>
2017-12-01 13:10:24 +0100

    Added cloud building of dockers

---
+++ CryoCloud/Modules/build_docker.py
@@ -0,0 +1,65 @@
+import subprocess
+import fcntl
+import os
+import select
+import re
+
+import CryoCore
+
+
+def process_task(worker, task):
+    args = task["args"]
+    if "target" not in args:
+        raise Exception("Missing target")
+
+    if "directory" not in args:
+        raise Exception("Missing directory")
+
+    cmd = ["docker", "build", "-t", args["target"], "."]
+
+    os.chdir(args["directory"])
+
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # We set the outputs as nonblocking
+    fcntl.fcntl(p.stdout, fcntl.F_SETFL, os.O_NONBLOCK)
+    fcntl.fcntl(p.stderr, fcntl.F_SETFL, os.O_NONBLOCK)
+
+    # read buffers
+    buf = {p.stdout: "", p.stderr: ""}
+
+    while not CryoCore.API.api_stop_event.isSet():
+        ready_fds = select.select([p.stdout, p.stderr], [], [], 1.0)[0]
+        for fd in ready_fds:
+            data = fd.read()
+            buf[fd] += data.decode("utf-8")
+
+        # process stdout - line by line
+        while buf[p.stdout].find("\n") > -1:
+            line, buf[p.stdout] = buf[p.stdout].split("\n", 1)
+            # print("stdout", line)
+            m = re.match("Step (\d+)/(\d+) : (.*)", line)
+            if m:
+                done, total = int(m.groups()[0]), int(m.groups()[1])
+                worker.status["done"] = done
+                worker.status["total"] = total
+                worker.status["current"] = m.groups()[2]
+                worker.status["progress"] = (int(100 * done / float(total)))
+                worker.log.info(line)
+
+        # process stderr
+        data = buf[p.stderr]
+        if data:
+            print("stderr", data)
+            buf[p.stderr] = ""
+
+        # check if the process is still running
+        _retval = p.poll()
+        if _retval is not None:
+            # Process exited
+            if _retval == 0:
+                break
+            # unexpected
+            raise Exception("Wrapped process '%s' exited with value %d" % (cmd, _retval))
+            break
+    return 100, None
---
+++ heads/build_docker.py
@@ -0,0 +1,63 @@
+
+from __future__ import print_function
+import CryoCloud
+
+
+description = """
+Build dockers
+"""
+
+supress = ["-o", "-r", "-f", "-t", "--steps", "--module"]
+
+
+# We have some additional arguments we'd like
+def addArguments(parser):
+    print("Adding arguments")
+    # parser.add_argument('parameters', type=str, nargs='+', help='Parameters to list/modify').completer = completer
+
+    parser.add_argument("targets", type=str, nargs='+', help="Dockers to build")
+    parser.add_argument("--node",
+                        dest="node",
+                        default=None,
+                        help="Target node for build")
+
+
+class Handler(CryoCloud.DefaultHandler):
+
+    def onReady(self, options):
+
+        print(dir(options))
+        self._tasks = []
+        self._taskid = 1
+
+        self.options = options
+
+        for target in self.options.targets:
+            print("Should build", target, "on", self.options.node)
+            task = {"target": target, "directory": self.options.input_dir}
+            self.head.add_job(1, self._taskid, task, module="build_docker", node=self.options.node)
+            self._tasks.append(self._taskid)
+            self._taskid += 1
+
+    def onAllocated(self, task):
+        print("Allocated", task["taskid"], "by node", task["node"], "worker", task["worker"])
+
+    def onCompleted(self, task):
+        print("Task completed:", task["taskid"], task["step"])
+        self._tasks.remove(task["taskid"])
+        print("Setting done", task["args"]["src"])
+        self.dir_monitor.setDone(task["args"]["src"])
+
+        print(len(self._tasks), "tasks left")
+        if len(self._tasks) == 0:
+            print("DONE")
+            self.head.stop()
+
+    def onTimeout(self, task):
+        print("Task timeout out, requeue")
+        self.head.requeue(task)
+
+    def onError(self, task):
+        print("Error for task", task)
+        # Notify someone, log the file as failed, try to requeue it or try to figure out what is wrong and how to fix it
+        pass
