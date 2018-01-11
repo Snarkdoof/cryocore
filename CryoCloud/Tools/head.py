7a3ee24639b491739a53b3090d9aeae098aaec1f
Njål Borch <njaal@njaal.net>
2018-01-11 08:20:19 +0100

    Add jobtype, forgot it earlier

--- CryoCloud/Tools/head.py
+++ CryoCloud/Tools/head.py
@@ -109,7 +109,7 @@ class HeadNode(threading.Thread):
         if expire_time is None:
             expire_time = self.options.max_task_time
         self._jobdb.add_job(step, taskid, args, expire_time=expire_time, module=module, node=node, priority=priority,
-                            modulepath=modulepath, workdir=workdir)
+                            modulepath=modulepath, workdir=workdir, jobtype=jobtype)
         if self.options.steps > 0 and self.options.tasks > 0:
             if step > self.status["progress"].size[0]:
                 self.status.new2d("progress", (self.options.steps, self.options.tasks),
