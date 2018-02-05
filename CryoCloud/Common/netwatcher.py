3cc2644ecb5c550f6b5c276ebd4f76a5f51a3e47
Njål Borch <njaal@njaal.net>
2018-02-05 08:31:37 +0100

    Netwatcher ready for testing

--- CryoCloud/Common/netwatcher.py
+++ CryoCloud/Common/netwatcher.py
@@ -5,6 +5,7 @@ import socketserver
 import threading
 import queue
 import json
+import jsonschema
 
 
 class MyWebServer(socketserver.TCPServer):
@@ -23,9 +24,13 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
             print("Failed to log", format, args)
 
     def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write("OK".encode("utf-8"))
+        if self.path == "/schema":
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(json.dumps(self.server.schema).encode("utf-8"))
+            return
+
+        self.send_error(404, "Could not find: " + self.request.path)
 
     def do_POST(self):
 
@@ -34,20 +39,32 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
             if len(data) == 0:
                 return self.send_error(500, "Missing body")
             info = json.loads(data.decode("utf-8"))
+
+            # Validate
+            if self.server.schema:
+                try:
+                    jsonschema.validate(info, self.server.schema)
+                except jsonschema.exceptions.ValidationError as ve:
+                    return self.send_error(400, "Invalid request: " + str(ve))
         except:
             self.server.inQueue.put(("error", "Bad JSON"))
             self.server.log.exception("Getting JSON post")
             return self.send_error(500, "Bad JSON")
 
         self.server.inQueue.put(("add", info))
-        self.send_response(200)
+        self.send_response(202)
         self.end_headers()
         self.flush_headers()
 
 
 class NetWatcher(threading.Thread):
-    def __init__(self, port=None, onAdd=None, onError=None, stop_event=None):
+    def __init__(self, port=None, onAdd=None, onError=None, stop_event=None, schema=None):
+        """
+        Schema must be a JSON schema for validating possible inputs
+        """
+
         threading.Thread.__init__(self)
+
         if stop_event:
             self._stop_event = stop_event
         else:
@@ -67,6 +84,7 @@ class NetWatcher(threading.Thread):
         self.server.timeout = 1.0
         self.server.log = self.log
         self.server.inQueue = queue.Queue()
+        self.server.schema = schema
 
         t = threading.Thread(target=self._handle_requests)
         t.start()
@@ -84,6 +102,9 @@ class NetWatcher(threading.Thread):
             self.server.socket.close()
         except:
             pass
+
+    def set_schema(self, schema):
+        self.server.schema = schema
 
     def stop(self):
         self._stop_event.set()
@@ -118,6 +139,32 @@ class NetWatcher(threading.Thread):
                 continue
 
 if __name__ == "__main__":
+
+    schema = {
+        "type": "object",
+        "properties": {
+            "product": {"type": "string"},
+            "configOverride": {
+                "type": "object",
+                "properties": {
+                    "pixelsize": {
+                        "type": "array",
+                        "items": {
+                            "type": "number"
+                        }
+                    },
+                    "multilook_factor": {
+                        "type": "number"
+                    }
+                }
+            }
+        }
+    }
+
+    jsonschema.validate({"product": "2", "cfonfigOverride": {"pixelsize": [1, "2"]}}, schema)
+    print("Validated fine!!!")
+    raise SystemExit()
+
     try:
         nw = NetWatcher(12345)
         nw.start()
